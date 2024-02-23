60ed819f06155dd8cc1d9c6859dbdb656c1b91ba
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2024-02-24 03:18:24 +0530

    refactor cursor anims 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 z-10">
       <div className="flex-1">
         <div className="dropdown dropdown-hover">
           <div tabIndex={0} role="button" className="btn m-1">
@@ -9,7 +9,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
+            className="dropdown-content z-[2] menu p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
               <a href="/">I have built everything from:</a>
@@ -67,7 +67,7 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            className="menu menu-sm dropdown-content mt-3 z-[2] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
               <a
